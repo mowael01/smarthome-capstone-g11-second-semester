fcf74397d0801de18034a13dc36bf453bd08b01e
Mohammed Wael Mohammed <mohammedwaelelshiekhmw@gmail.com>
2024-03-18 15:13:40 +0200

    removed app.js and added app.tsx

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import { LineChart, Grid } from "react-native-svg-charts";
-import { Circle, G, Line, Rect } from "react-native-svg";
+import { StyleSheet, Text, View, Dimensions } from "react-native";
+import React from "react";
+import MyBarChart from "./components/graph";
 
 const data = [50, 10, 40, 95, 45, 60, 70, 30, 200, 170];
 
@@ -13,26 +13,13 @@ export default function App() {
           style={{
             fontSize: 20,
             color: "#2e2e2e",
-            // backgroundColor: "red",
-            padding: "10px",
+            // padding: "10px",
             width: "calc(100% - 20px)",
           }}
         >
           Smart Home Feed
         </Text>
       </View>
-      <LineChart
-        style={{ height: 200, padding: 20 }}
-        data={data}
-        svg={{
-          stroke: "rgb(134, 65, 244)",
-
-          strokeWidth: 2,
-        }}
-        contentInset={{ top: 20, bottom: 20 }}
-      >
-        <Grid />
-      </LineChart>
       <StatusBar style="auto" />
     </View>
   );
@@ -41,8 +28,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     marginTop: 40,
+    width: "100%",
     flex: 1,
     backgroundColor: "#fff",
     alignItems: "center",
+  },
+  topHome: {
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "red",
+    height: 100,
   },
 });
