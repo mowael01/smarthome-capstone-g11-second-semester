1ee88be963710060f6bf4b8ab77c8a2a47f7aae9
Mohammed Wael Mohammed <mohammedwaelelshiekhmw@gmail.com>
2024-03-18 08:49:43 +0200

    initilaized the main project

--- App.js
+++ App.js
@@ -1,10 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.topHome}>
+        <Text
+          style={{
+            fontSize: 20,
+            color: "#2e2e2e",
+            backgroundColor: "red",
+            width: "100%",
+          }}
+        >
+          Welcome to Your Smart Home Feed App.
+        </Text>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -12,9 +23,9 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
+    marginTop: 40,
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
   },
 });
