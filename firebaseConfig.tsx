257953de12592eb4963ef3efe40e040efc20f6f4
Mohammed Wael Mohammed <mohammedwaelelshiekhmw@gmail.com>
2024-03-24 17:31:10 +0200

    Replaces Firestore with firebase realtime database

--- firebaseConfig.tsx
+++ firebaseConfig.tsx
@@ -3,8 +3,7 @@ import { initializeApp } from "firebase/app";
 // Optionally import the services that you want to use
 import { getAuth } from "firebase/auth";
 // import {...} from "firebase/database";
-import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage";
+import { getDatabase } from "firebase/database";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -18,10 +17,13 @@ const firebaseConfig = {
   messagingSenderId: "280217500857",
   appId: "1:280217500857:web:70e5df9abca5c31660ef68",
   measurementId: "G-G97B311M2H",
+  databaseURL: "https://DATABASE_NAME.firebaseio.com",
 };
 
 // Initialize Firebase
 export const FirebaseApp = initializeApp(firebaseConfig);
-export const FireStoreDB = getFirestore(FirebaseApp);
+
 export const FirebaseAUTH = getAuth(FirebaseApp);
-export const FireStorage = getStorage(FirebaseApp);
+
+// Initialize Realtime Database and get a reference to the service
+const database = getDatabase(app);
