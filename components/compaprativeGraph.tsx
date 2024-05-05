6d428d1cbd3bd9c5efef5292df8c85ed046c2eaa
Mohammed Wael Elshiekh <mohammedwaelelshiekhmw@gmail.com>
2024-05-06 02:44:02 +0300

    finished the graph of inside and outside and it works perfectly

--- components/compaprativeGraph.tsx
+++ components/compaprativeGraph.tsx
@@ -139,6 +139,7 @@ import {
   Text as SkiaText,
   useFont,
   vec,
+  AnimatedProp,
 } from "@shopify/react-native-skia";
 import {
   SafeAreaView,
@@ -158,22 +159,12 @@ import { Database } from "../firebaseConfig";
 import { getAuth } from "firebase/auth";
 import * as Notifications from "expo-notifications";
 
-// import { useDarkMode } from "react-native-dark";
-// import * as Haptics from "expo-haptics";
-// import inter from "../assets/inter-medium.ttf";
-// import { AnimatedText } from "../components/AnimatedText";
-// import { appColors } from "./consts/colors";
-// import data from "../data/stockprice/tesla_stock.json";
-// import { InfoCard } from "../components/InfoCard";
-// import { descriptionForRoute } from "./consts/routes";
 
 
 export default function ComparativeGraph(props) {
   const font = useFont(require("../assets/fonts/Nunito-Italic-VariableFont_wght.ttf"), 12)
   const { state: firstTouch, isActive: isFirstPressActive } =
-    useChartPressState({ x: 0, y: { y: 0 } });
-  const { state: secondTouch, isActive: isSecondPressActive } =
-    useChartPressState({ x: 0, y: { y: 0 } });
+    useChartPressState({ x: 0, y: { y: 0, z: 0 } });
 
   async function sendPushNotification(title, body) {
     await Notifications.scheduleNotificationAsync({
@@ -199,12 +190,32 @@ export default function ComparativeGraph(props) {
   const [year, setyear] = React.useState({})
 
   const [info, setInfo] = React.useState([
-    { x: 0, y: 1 }, //0
-    { x: 1, y: 1 }, //1
-    { x: 2, y: 1 }, //2
-    { x: 3, y: 1 }, //3
-    { x: 4, y: 1 }, //4
-    { x: 5, y: 1 } //5
+    { x: 0, y: 1, z: 2 }, //0
+    { x: 1, y: 1, z: 2 }, //1
+    { x: 2, y: 1, z: 2 }, //2
+    { x: 3, y: 1, z: 2 }, //3
+    { x: 4, y: 1, z: 2 }, //4
+    { x: 5, y: 1, z: 2 }, //5
+    { x: 6, y: 1, z: 2 }, //0
+    { x: 7, y: 1, z: 2 }, //1
+    { x: 8, y: 1, z: 2 }, //2
+    { x: 9, y: 1, z: 2 }, //3
+    { x: 10, y: 1, z: 2 }, //4
+    { x: 11, y: 1, z: 2 }, //5
+    { x: 12, y: 1, z: 2 }, //0
+    { x: 13, y: 1, z: 2 }, //1
+    { x: 14, y: 1, z: 2 }, //2
+    { x: 15, y: 1, z: 2 }, //3
+    { x: 16, y: 1, z: 2 }, //4
+    { x: 17, y: 1, z: 2 }, //5
+    { x: 18, y: 1, z: 2 }, //0
+    { x: 19, y: 1, z: 2 }, //1
+    { x: 20, y: 1, z: 2 }, //2
+    { x: 21, y: 1, z: 2 }, //3
+    { x: 22, y: 1, z: 2 }, //4
+    { x: 23, y: 1, z: 2 }, //5
+
+
   ]);
 
 
@@ -217,24 +228,30 @@ export default function ComparativeGraph(props) {
       });
       console.log(newD);
 
-      setInfo(newD);
+      // setInfo(newD);
+      return newD.sort((a, b) => a.x - b.x);
+    }).then((newD) => {
+      fetch(
+        "https://api.weatherapi.com/v1/forecast.json?key=4b9054e1f4214ea9913140905242504&q=cairo&days=7&aqi=no&alerts=no"
+      )
+        .then(data => data.json())
+        .then(data => {
+          setCurrent(data.current);
+          setDay(data.forecast.forecastday[0].hour.map(ele => {
+            return ({ x: +ele.time.slice(-5, -3), y: ele.temp_c }) //[ele.time.slice(-5),ele]
+          }));
+          const newInfo = info.map((ele, index) => {
+            return { x: ele.x, y: +newD[index].y, z: +data.forecast.forecastday[0].hour[index].temp_c }
+          });
+          console.log("test" + newInfo);
+
+          setInfo(newInfo)
+          console.log(`day:`);
+          console.log(info);
+
+          setWeek({})
+        })
     })
-    fetch(
-      "https://api.weatherapi.com/v1/forecast.json?key=4b9054e1f4214ea9913140905242504&q=cairo&days=7&aqi=no&alerts=no"
-    )
-      .then(data => data.json())
-      .then(data => {
-        setCurrent(data.current);
-        setDay(data.forecast.forecastday[0].hour.map(ele => {
-          return ({ x: +ele.time.slice(-5, -3), y: ele.temp_c }) //[ele.time.slice(-5),ele]
-        }));
-        console.log(`day:`);
-        console.log(day);
-
-
-        setWeek({})
-      })
-
   }, [])
 
 
@@ -246,9 +263,9 @@ export default function ComparativeGraph(props) {
           data={info}
           domain={{ y: [0, 40], x: [0, 23] }}
           xKey="x"
-          yKeys={["y"]}
-          chartPressState={[firstTouch, secondTouch]}
-          curve="linear"
+          yKeys={["y", "z"]}
+          chartPressState={[firstTouch]}
+          // curve="natural"
           axisOptions={{
             font,
             tickCount: { x: 12, y: 10 },
@@ -260,26 +277,36 @@ export default function ComparativeGraph(props) {
               {isFirstPressActive ? <ActiveValueIndicator
                 xPosition={firstTouch.x.position}
                 yPosition={firstTouch.y.y.position}
+                zPosition={firstTouch.y.z.position}
                 bottom={chartBounds.bottom}
                 top={chartBounds.top}
-                activeValue={firstTouch.y.y.value}
+                activeValueY={firstTouch.y.y.value}
+                activeValueZ={firstTouch.y.z.value}
+                valueX={firstTouch.x}
                 textColor={"red"}
-                lineColor={"#d4d4d8"}
+                lineColor={"#F0ED00"}
                 // @ts-ignore
-                indicatorColor={"#085408"}
+                indicatorColor={"#F0ED00"}
               /> : ""}
             </>
           )}
         >
           {({ chartBounds, points }) => (
             <>
-              <StockArea
-                points={points.y}
-                isWindowActive={isFirstPressActive && isSecondPressActive}
+              <CustomArea
+                color="#3B5970"
+                points={points.z}
                 startX={firstTouch.x.position}
-                endX={secondTouch.x.position}
+                {...chartBounds}
+
+              />
+              <CustomArea
+                color="#0005DB"
+                points={points.y}
+                startX={firstTouch.x.position}
                 {...chartBounds}
               />
+
             </>
           )}
         </CartesianChart>
@@ -291,62 +318,29 @@ export default function ComparativeGraph(props) {
 /**
  * Show the line/area chart for the stock price, taking into account press state.
  */
-const StockArea = ({
+const CustomArea = ({
   points,
-  isWindowActive,
-  startX,
-  endX,
-  left,
-  right,
   top,
   bottom,
+  color
 }: {
+  color: AnimatedProp<String>;
   points: PointsArray;
-  isWindowActive: boolean;
   startX: SharedValue<number>;
-  endX: SharedValue<number>;
 } & ChartBounds) => {
-  const { path: areaPath } = useAreaPath(points, bottom);
-  const { path: linePath } = useLinePath(points);
-
-  const backgroundClip = useDerivedValue(() => {
-    const path = Skia.Path.Make();
-
-    if (isWindowActive) {
-      path.addRect(Skia.XYWHRect(left, top, startX.value - left, bottom - top));
-      path.addRect(
-        Skia.XYWHRect(endX.value, top, right - endX.value, bottom - top),
-      );
-    } else {
-      path.addRect(Skia.XYWHRect(left, top, right - left, bottom - top));
-    }
-
-    return path;
-  });
-
-  const windowClip = useDerivedValue(() => {
-    if (!isWindowActive) return Skia.Path.Make();
-
-    const path = Skia.Path.Make();
-    path.addRect(
-      Skia.XYWHRect(startX.value, top, endX.value - startX.value, bottom - top),
-    );
-    return path;
-  });
-
-  const gradColors = "#ff0000"
-
-  const windowLineColor = "#00ff00"
+  const { path: areaPath } = useAreaPath(points, bottom, { curveType: "natural" });
+  const { path: linePath } = useLinePath(points, { curveType: "natural" });
 
   return (
     <>
       {/* Base */}
-      <Group clip={backgroundClip} opacity={isWindowActive ? 0.3 : 1}>
-        <Path path={areaPath} style="fill">
+      <Group>
+        <Path path={areaPath} style="fill" >
           <LinearGradient
             start={vec(0, 0)}
             end={vec(top, bottom)}
-            colors={["#ff0000", "#ff000033"]}
+            // @ts-ignore
+            colors={[color, color + "33"]}
 
           />
         </Path>
@@ -354,8 +348,9 @@ const StockArea = ({
           path={linePath}
           style="stroke"
           strokeWidth={2}
+          // @ts-ignore
           color={
-            "red"
+            color
           }
         />
       </Group>
@@ -366,9 +361,12 @@ const StockArea = ({
 const ActiveValueIndicator = ({
   xPosition,
   yPosition,
+  zPosition,
   top,
   bottom,
-  activeValue,
+  valueX,
+  activeValueY,
+  activeValueZ,
   textColor,
   lineColor,
   indicatorColor,
@@ -376,7 +374,10 @@ const ActiveValueIndicator = ({
 }: {
   xPosition: SharedValue<number>;
   yPosition: SharedValue<number>;
-  activeValue: SharedValue<number>;
+  zPosition: SharedValue<number>;
+  valueX: any;
+  activeValueY: SharedValue<number>;
+  activeValueZ: SharedValue<number>;
   bottom: number;
   top: number;
   textColor: string;
@@ -390,32 +391,43 @@ const ActiveValueIndicator = ({
   const end = useDerivedValue(() =>
     vec(xPosition.value, top + 1.5 * FONT_SIZE + topOffset),
   );
+
   // Text label
   const activeValueDisplay = useDerivedValue(
-    () => activeValue.value.toFixed(2) + ` C`,
+    () => `Inside: ${activeValueY.value.toFixed(2)}C, Outside: ${activeValueZ.value.toFixed(2)} at ${xPosition.value}:00`,
   );
   const activeValueWidth = useDerivedValue(
     () => font?.getTextWidth(activeValueDisplay.value) || 0,
   );
+
   const activeValueX = useDerivedValue(
     () => xPosition.value - activeValueWidth.value / 2,
   );
 
+
+
   return (
     <>
       <SkiaLine p1={start} p2={end} color={lineColor} strokeWidth={1} />
-      <Circle cx={xPosition} cy={yPosition} r={10} color={indicatorColor} />
+      <Circle cx={xPosition} cy={yPosition} r={6} color={indicatorColor} />
       <Circle
         cx={xPosition}
         cy={yPosition}
-        r={8}
+        r={4}
+        color="hsla(0, 0, 100%, 0.25)"
+      />
+      <Circle cx={xPosition} cy={zPosition} r={6} color={indicatorColor} />
+      <Circle
+        cx={xPosition}
+        cy={zPosition}
+        r={4}
         color="hsla(0, 0, 100%, 0.25)"
       />
       <SkiaText
         color={textColor}
         font={font}
         text={activeValueDisplay}
-        x={activeValueX}
+        x={65}
         y={top + FONT_SIZE + topOffset}
       />
     </>
